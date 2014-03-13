01ae60a3ad122fc1794d922696ccbfa78accadbb
Slack Unfair <aaa4bbb@trash-mail.com>
2014-03-13 16:38:01 +0100

    Added reading from arg[1] to main

--- project/src/tool_project/Main.java
+++ project/src/tool_project/Main.java
@@ -1,9 +1,31 @@
 package tool_project;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStream;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		if( args.length != 2){
+			System.exit(1);
+		}
+		
+		try {
+			ANTLRFileStream filestream = new ANTLRFileStream( 
+										Arrays.deepToString(Files.readAllLines( Paths.get( args[1] ), StandardCharsets.UTF_8).toArray()));
+			
+			//TokenStream tokens = new CommonTokenStream(filestream);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
