ab1133988e6e871e6f29cb521f8e4812f8993a8b
Slack Unfair <SlackUnfairguy@users.noreply.github.com>
2014-04-12 21:39:38 +0200

    Added global attribute

--- project/src/compiler/Variable.java
+++ project/src/compiler/Variable.java
@@ -3,10 +3,12 @@ package compiler;
 public class Variable {	
 	private Integer id;
 	private Datatype type;
+	private boolean global;
 	
-	public Variable(Integer pId, Datatype pType){
+	public Variable(Integer pId, Datatype pType, boolean pIsGlobal){
 		this.id = pId;
 		this.type = pType;
+		this.global = pIsGlobal;
 	}
 
 	public Integer getId() {
@@ -23,5 +25,9 @@ public class Variable {
 
 	public void setType(Datatype type) {
 		this.type = type;
+	}
+	
+	public boolean isGlobalVariable(){
+		return this.global;
 	}
 }
