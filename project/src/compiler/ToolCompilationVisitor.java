55d5f56d2ac351545878661dbbac5b9a8163af54
Babbsdrebbler <babbsdrebbler@hotmail.de>
2014-04-18 16:10:19 +0200

    Fixed toStr, what did I think when I added it...

--- project/src/compiler/ToolCompilationVisitor.java
+++ project/src/compiler/ToolCompilationVisitor.java
@@ -34,7 +34,7 @@ public class ToolCompilationVisitor extends ToolBaseVisitor<String> {
 			{
 				put("return", "return");
 				put("sprich", "getstatic java/lang/System/out Ljava/io/PrintStream;"+ "swap\n" + "\n" + "invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V");
-				put("toStr", "getstatic java/lang/Integer Ljava/lang/Integer;" + "\n" + "invokevirtual java/lang/Integer/toStr(Ljava/lang/Integer;)L");
+				put("toStr", " invokestatic java/lang/Integer/toString(I)Ljava/lang/String;");
 			}
 		};
 	}
